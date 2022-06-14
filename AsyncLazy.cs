eb18837d8a9665d2b18daaa76ce47b9929c385a3
Marvin Botchway <marvinbotchway@outlook.com>
2022-06-14 11:38:29 +0000

    Add Lasy Loading class

---
+++ AsyncLazy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList
+{
+    public class AsyncLazy<T>
+    {
+        readonly Lazy<Task<T>> instance;
+
+        public AsyncLazy(Func<T> factory)
+        {
+            instance = new Lazy<Task<T>>(() => Task.Run(factory));
+        }
+
+
+        public TaskAwaiter<T> GetAwaiter()
+        {
+            return instance.Value.GetAwaiter();
+        }
+
+        public void Start()
+        {
+            var unused = instance.Value;
+        }
+    }
+}
